d36b74c817aec33df9e9083ed54d73ea24e20b8d
Your Name <amanisabrah@gmail.com>
2024-05-23 16:24:01 +0300

    Add project files.

---
+++ WebApplication3/DB/DB_Models/USE_User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DB;
+
+public partial class USE_User
+{
+    public int USE_User_ID { get; set; }
+
+    public string? USE_User_Name { get; set; }
+
+    public string? USE_User_Email { get; set; }
+
+    public int? USE_User_Gender { get; set; }
+
+    public string? USE_User_Phone { get; set; }
+
+    public string USE_User_Password { get; set; } = null!;
+}
